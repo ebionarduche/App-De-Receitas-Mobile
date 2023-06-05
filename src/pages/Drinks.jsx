7989a9a8ea498ec4d394893159a15f67c9f03b9b
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-06-05 19:16:36 -0300

    Requisito 17 - Importando Footer para as rotas(meals, drinks e profile)

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,9 +1,11 @@
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Drinks() {
   return (
     <div>
       <Header title="Drinks" btnProfile btnSearch />
+      <Footer />
     </div>
   );
 }
