86366c7c325c66c96260479329eb4574f290c1b7
Ebio Narduche <ebionarduche77@gmail.com>
2023-06-05 16:17:48 -0300

    Router & Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Router from './components/Router';
 
 function App() {
   return (
     <div className="meals">
       <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <Router />
     </div>
   );
 }
