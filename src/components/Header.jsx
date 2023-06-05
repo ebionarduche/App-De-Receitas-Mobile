e92be403348a63190679483de4a3a67855cc4217
Ebio Narduche <ebionarduche77@gmail.com>
2023-06-05 20:18:12 -0300

    Requisito 09 Togle Search Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ title, btnProfile, btnSearch }) {
+  const [searchBar, SetSearchBar] = useState(false);
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/profile');
+  };
+
+  const TogleSearchBar = () => {
+    if (searchBar === false) {
+      SetSearchBar(true);
+    } else {
+      SetSearchBar(false);
+    }
+  };
+
   return (
     <div>
       <h1 data-testid="page-title">{title}</h1>
 
       {btnProfile && (
-        <button type="button" data-testid="profile-top-btn">
+        <button
+          type="button"
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          onClick={ handleClick }
+        >
           <img src={ profileIcon } alt="profile-icon" />
         </button>
       )}
 
       {btnSearch && (
-        <button type="button" data-testid="search-top-btn">
+        <button
+          type="button"
+          data-testid="search-top-btn"
+          src={ searchIcon }
+          onClick={ TogleSearchBar }
+        >
           <img src={ searchIcon } alt="search-icon" />
         </button>
       )}
+      {
+        searchBar && <SearchBar />
+      }
     </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        name=""
+        id=""
+        data-testid="search-input"
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
