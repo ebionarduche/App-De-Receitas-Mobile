fe12c7dec1f4eed428f776fbfc1ad0c92c43b8ce
Ebio Narduche <ebionarduche77@gmail.com>
2023-06-05 16:17:48 -0300

    Router & Pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />);
+  .render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function Recipes() {
+  return (
+    <div>
+      <Header />
+      <h1>Recipes</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default Recipes;
