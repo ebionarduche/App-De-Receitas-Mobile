073f38aa1d6ef710161e485c35cbe55b2f2f61b2
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-06-06 14:44:40 -0300

    [Grupo 6]Footer - Iniciando testes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import React from 'react';
 import drink from '../images/drinkIcon.svg';
 import food from '../images/mealIcon.svg';
 import '../style/Footer.css';
 
 function Footer() {
+  const history = useHistory();
+  const handleClick = (url) => {
+    history.push(url);
+  };
+
   return (
     <footer
       data-testid="footer"
@@ -11,12 +18,14 @@ function Footer() {
       <button
         data-testid="drinks-bottom-btn"
         src={ drink }
+        onClick={ () => handleClick('/drinks') }
       >
         <img src={ drink } alt="" />
       </button>
       <button
         data-testid="meals-bottom-btn"
         src={ food }
+        onClick={ () => handleClick('/meals') }
       >
         <img src={ food } alt="" />
       </button>
