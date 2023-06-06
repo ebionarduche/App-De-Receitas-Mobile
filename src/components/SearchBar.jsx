d2837b77a13beb081f9dfdbc8aeadf636e321c90
hongllqi <hongli1302@gmail.com>
2023-06-06 04:03:26 -0300

    [Grupo 6] SearchBar - Req 10, 11, 12

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,65 @@
+import React, { useContext } from 'react';
+import SearchContext from '../context/SearchContext';
+
 function SearchBar() {
+  const {
+    searchApi,
+  } = useContext(SearchContext);
+
+  const searchHandle = () => {
+    const searchInput = document.querySelector('input[name=\'search\']:checked').value;
+    const searchWord = document.querySelector('input[name=\'searchWord\']').value;
+
+    console.log(searchInput);
+    switch (searchInput) {
+    case 'ingredient':
+      searchApi(`filter.php?i=${searchWord}`);
+      break;
+    case 'name':
+      searchApi(`search.php?s=${searchWord}`);
+      break;
+    case 'first-letter':
+      if (searchWord.length > 1) {
+        global.alert('Your search must have only 1 (one) character');
+      } else { searchApi(`search.php?f=${searchWord}`); }
+      break;
+    default:
+      return '';
+    }
+  };
+
   return (
     <div>
       <input
         type="text"
-        name=""
+        name="searchWord"
         id=""
         data-testid="search-input"
       />
+      <input
+        type="radio"
+        name="search"
+        value="ingredient"
+        data-testid="ingredient-search-radio"
+      />
+      <input
+        type="radio"
+        name="search"
+        value="name"
+        data-testid="name-search-radio"
+      />
+      <input
+        type="radio"
+        name="search"
+        value="first-letter"
+        data-testid="first-letter-search-radio"
+      />
+      <button
+        data-testid="exec-search-btn"
+        onClick={ searchHandle }
+      >
+        Buscar
+      </button>
     </div>
   );
 }
