2d387e7df05e2bcf8347a81b196ec6a313185d9d
Taline Chuman <talinechuman@hotmail.com>
2023-06-06 15:43:24 -0300

    Tela de login Requisitos 02,03,04,05,06

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,7 +1,6 @@
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouter } from './renderWith';
-// import userEvent from '@testing-library/user-event';
-// import { act } from 'react-dom/test-utils';
 import App from '../App';
 
 describe('Testando a renderização na tela', () => {
@@ -16,5 +15,22 @@ describe('Testando a renderização na tela', () => {
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  it('testando rota após clicar no button', () => {
+    const { history } = renderWithRouter(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const button = screen.getByRole('button', { name: /entrar/i });
+    expect(button).toBeDisabled();
+
+    fireEvent.change(emailInput, { target: { value: 'trybee@teste.com' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    expect(button).toBeEnabled();
+
+    userEvent.click(button);
+    expect(history.location.pathname).toBe('/meals');
   });
 });
