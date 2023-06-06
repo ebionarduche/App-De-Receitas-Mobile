2d708b6c3ab0036be9aedc7856414db53dce3a9e
Taline Chuman <talinechuman@hotmail.com>
2023-06-06 14:40:17 -0300

    Atualizando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Login() {
   const [values, setValues] = useState({
