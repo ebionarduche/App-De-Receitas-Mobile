84e0ffe50841d1d9891fdaf30e38aa1f30fcf48b
hongllqi <hongli1302@gmail.com>
2023-06-12 19:58:09 -0300

    adicionado nacionalidade em caso de Meals. Requisito 45

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function RecipeCard({ index, recipe }) {
+  const history = useHistory();
+  const { pathname } = history.location;
   const {
-    thumbnail,
+    image,
     category,
+    nationality,
     name,
-    dateDone,
-    strTags,
+    doneDate,
+    tags,
   } = recipe;
 
   // As Tags vem da API numa string separadas por vírgula
-  const tagsP = strTags.split(',').splice(0, 2).map((tag, ind) => (
+  const tagsP = tags.split(',').splice(0, 2).map((tag, ind) => (
     <p
       key={ tag + index + ind }
       data-testid={ `${index}-${tag}-horizontal-tag` }
@@ -23,14 +27,14 @@ export default function RecipeCard({ index, recipe }) {
   return (
     <div>
       <img
-        src={ thumbnail }
+        src={ image }
         alt={ name }
         data-testid={ `${index}-horizontal-image` }
       />
       <p
         data-testid={ `${index}-horizontal-top-text` }
       >
-        { category }
+        { pathname === '/drinks' ? category : `${nationality} - ${category}` }
       </p>
       <p
         data-testid={ `${index}-horizontal-name` }
@@ -40,14 +44,14 @@ export default function RecipeCard({ index, recipe }) {
       <p
         data-testid={ `${index}-horizontal-done-date` }
       >
-        { dateDone }
+        { doneDate }
       </p>
       { tagsP }
-      <button
+      <img
         data-testid={ `${index}-horizontal-share-btn` }
-      >
-        Compartilhar
-      </button>
+        src="src/images/shareIcon.svg"
+        alt="share"
+      />
     </div>
   );
 }
@@ -55,10 +59,11 @@ export default function RecipeCard({ index, recipe }) {
 RecipeCard.propTypes = {
   index: PropTypes.number.isRequired,
   recipe: PropTypes.shape({
-    thumbnail: PropTypes.string,
+    image: PropTypes.string,
     category: PropTypes.string,
+    nationality: PropTypes.string,
     name: PropTypes.string,
-    dateDone: PropTypes.string,
-    strTags: PropTypes.string,
+    doneDate: PropTypes.string,
+    tags: PropTypes.string,
   }).isRequired,
 };
--- src/context/ProgressProvider.js
+++ src/context/ProgressProvider.js
@@ -7,18 +7,35 @@ export default function ProgressProvider({ children }) {
   const [doneRecipes, setDoneRecipes] = useState([]);
 
   useEffect(() => {
-    const initialDoneRecipe = {
-      // Receita feita inicial
-      // Observar a estrutura do objeto
-      thumbnail: 'http://imagemDaReceita.com',
-      category: 'Categoria da receita',
-      name: 'Nome da receita',
-      dateDone: 'Data que a receita foi feita',
-      strTags: 'Pasta,Curry,Tag3,TagN',
-    };
+    // Array de receitas feitas inicial
+    const initialDoneRecipes = [
+      {
+        // Observar a estrutura do objeto
+        id: '52771',
+        type: 'meal',
+        nationality: 'Italian',
+        category: 'Vegetarian',
+        alcoholicOrNot: '',
+        name: 'Spicy Arrabiata Penne',
+        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+        doneDate: '23/06/2020',
+        tags: 'Pasta,Curry',
+      },
+      {
+        id: '178319',
+        type: 'drink',
+        nationality: '',
+        category: 'Cocktail',
+        alcoholicOrNot: 'Alcoholic',
+        name: 'Aquamarine',
+        image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+        doneDate: '23/06/2020',
+        tags: '',
+      },
+    ];
     // Iniciando o state com um array contendo apenas este mesmo recipe duas vezes
     // (Requisito pediu assim)
-    setDoneRecipes([initialDoneRecipe, initialDoneRecipe]);
+    setDoneRecipes(initialDoneRecipes);
   }, []);
 
   const initialState = useMemo(() => ({
