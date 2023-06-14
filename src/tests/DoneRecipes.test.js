1562b5d1c8ebfdb3378628cfa8541e159f8c028b
Ebio Narduche <ebionarduche77@gmail.com>
2023-06-14 17:38:14 -0300

    Testes da Done 80%

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouterAndContext from './helpers/RenderWithContext';
 import App from '../App';
 
@@ -29,25 +30,42 @@ const newDoneRecipes = [
 ];
 
 describe('DoneRecipes', () => {
-  beforeEach(() => {
-    const localStorageMock = {
-      getItem: jest.fn().mockReturnValue(JSON.stringify(newDoneRecipes)),
-      setItem: jest.fn(),
-      removeItem: jest.fn(),
-      clear: jest.fn(),
-    };
-    global.localStorage = localStorageMock;
-  });
-
+  const doneRecipe = '/done-recipes';
   afterEach(() => {
-    global.localStorage = undefined;
+    localStorage.clear();
   });
 
-  it('renderiza a página de Receitas Feitas com os cards de receitas', async () => {
-    renderWithRouterAndContext(<App />, '/done-recipes');
+  it('renderiza a página de Receitas Feitas com os cards de receitas', () => {
+    localStorage.setItem('doneRecipes', JSON.stringify(newDoneRecipes));
+    renderWithRouterAndContext(<App />, doneRecipe);
 
-    await waitFor(() => {
-      screen.getByRole('img', { name: /spicy arrabiata penne/i });
-    });
+    screen.getByText(/italian - vegetarian/i);
+    screen.getByText(/alcoholic/i);
+  });
+  it('Testa Se os botões de filtros funcionam corretamente ', () => {
+    localStorage.setItem('doneRecipes', JSON.stringify(newDoneRecipes));
+    renderWithRouterAndContext(<App />, doneRecipe);
+
+    const meals = screen.getByRole('button', { name: /meals/i });
+    userEvent.click(meals);
+
+    expect(screen.queryByText(/alcoholic/i)).not.toBeInTheDocument();
+
+    const drinks = screen.getByRole('button', { name: /drinks/i });
+    userEvent.click(drinks);
+
+    expect(screen.queryByText(/italian - vegetarian/i)).not.toBeInTheDocument();
+
+    const all = screen.getByRole('button', { name: /all/i });
+    userEvent.click(all);
+
+    screen.getByText(/italian - vegetarian/i);
+    screen.getByText(/alcoholic/i);
+  });
+  it('renderiza a página de Receitas Feitas com os cards de receitas', () => {
+    renderWithRouterAndContext(<App />, doneRecipe);
+
+    const horizontalTopText = screen.queryByTestId('0-horizontal-top-text');
+    expect(horizontalTopText).not.toBeInTheDocument();
   });
 });
