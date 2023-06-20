2a845f75fbc3470ec44432b5908cfbf8677eaa4f
Ebio Narduche <ebionarduche77@gmail.com>
2023-06-20 13:07:35 -0300

    Page Perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,10 @@
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../style/Profile.css';
+import check from '../images/check.svg';
+import logout from '../images/logout.svg';
+import yellowHeart from '../images/yellowHeart.png';
 
 function Profile() {
   const email = JSON.parse(localStorage.getItem('user'));
@@ -18,45 +22,47 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" btnProfile />
-      <div>
-        {email && (
-          <h1
-            data-testid="profile-email"
-          >
-            {email.email}
+      <section className="profile-container">
+        <div>
+          {email && (
+            <h1 data-testid="profile-email">
+              {email.email}
 
-          </h1>
-        )}
-      </div>
-      <div>
+            </h1>
+          )}
+        </div>
         <button
           data-testid="profile-done-btn"
           onClick={ () => handleClick('/done-recipes') }
         >
-          Done Recipes
+          <div>
+            <img src={ check } alt="" width="39px" />
+            Done Recipes
+          </div>
         </button>
-      </div>
-      <div>
+
         <button
           data-testid="profile-favorite-btn"
           onClick={ () => handleClick('/favorite-recipes') }
         >
-          Favorite Recipes
+          <div>
+            <img src={ yellowHeart } alt="" width="42px" />
+            Favorite Recipes
+          </div>
         </button>
-      </div>
-      <div>
 
         <button
           data-testid="profile-logout-btn"
           onClick={ () => handleLocalStorage() }
         >
-          Logout
+          <div>
+            <img src={ logout } alt="" />
+            Logout
+          </div>
         </button>
-      </div>
-      <div>
 
-        <Footer />
-      </div>
+      </section>
+      <Footer />
     </div>
   );
 }
